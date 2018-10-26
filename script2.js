2ab28dc09bc93bd428b7472d878c02292d0022fb
xgingerbreadmanx <rjjohnson1204@gmail.com>
2018-10-25 21:05:35 -0700

    pair programming

---
+++ script2.js
@@ -0,0 +1,119 @@
+  'use strict';
+
+  console.log('userName', userName);
+
+  var userName = prompt('Welcome to the site. What is your name?');
+  alert(' Nice to meet you ' + userName + '.');
+  console.log('userName', userName);
+
+  alert('please respond to the following five questions with yes or no');
+
+function question( varible1, varible2) {
+if ( varible1 === varible2 ){
+alert( 'correct'); }
+else { alert ('wrong') }
+console.log(' ')
+}
+
+var candy = prompt('Do I like candy?');
+var res = 'no';
+question(candy, res);
+
+    function question( varible1, varible2) {
+  if ( varible1 === varible2 ){
+  alert( 'correct'); }
+  else { alert ('wrong') }
+  console.log(' ')
+  }
+  var color = prompt('Is my favorite color blue?');
+  var res = 'yes'
+  question( color, res);
+
+function question(varible1, varible2) {
+  if (varible1 === varible2) {
+    alert('correct');
+  } else {
+    alert('wrong');
+  }
+  console.log(' ');
+}
+var pie = prompt('Do I like apple pie?');
+var res = 'yes';
+question(pie, res);
+
+function question(varible1, varible2) {
+  if (varible1 === varible2) {
+    alert('correct');
+  } else {
+    alert('wrong');
+  }
+  console.log(' ');
+}
+var hand = prompt('Am I left handed?');
+var res = 'no';
+question(hand, res);
+
+function question(varible1, varible2) {
+  if (varible1 === varible2) {
+    alert('correct');
+  } else {
+    alert('wrong');
+  }
+  console.log(' ');
+}
+var height = prompt('Am I six feet tall?');
+var res = 'no';
+question(height, res);
+
+
+// var favoriteNumber = 3
+// var counter = 0
+// console.log('numberGuess', numberGuess);
+// var numberGuess = parseInt(prompt(' What is my favorite number? You have four guesses '));
+// console.log('numberGuess', numberGuess);
+
+// while (numberGuess !== favoriteNumber && counter < 3) {
+//   counter++;
+
+//   // if (numberGuess === 3) {
+//   //   alert('Correct')
+//   }
+//   if (numberGuess > favoriteNumber) {
+//     parseInt(alert(' Close, a little lower!'));
+//   }
+//   else if (numberGuess < 3) {
+//     parseInt(alert(' Close, a little higher!'));
+//   // } else{
+//   //   numberGuess = parseInt(prompt( 'correct'));
+//   }
+//   // else(alert( 'Correct'));
+//   //  if (numberGuess === 3){
+//   //     alert( 'Correct')
+
+// // numberGuess = parseInt(prompt(numberGuess + ' is not a number'));
+
+//     // else(numberGuess){
+//     //     alert( 'Correct')
+// // }
+
+// // console.log('numberGuess', numberGuess);
+// // var favoriteNumber = 3
+// // var numberGuess  = prompt(' What is my favorite number? ');
+// // console.log('numberGuess', numberGuess);
+
+// // if (numberGuess === 3){
+// //     alert( 'correct')
+// // }
+// // else if ( numberGuess > 3){
+// // alert( ' Close, a little lower!')
+// // }
+// // else if ( numberGuess < 3){
+// //     alert( ' Close, a little higher!')
+// //     }
+// //     numberGuess =
+// // if (i++ ===  ) break;
+
+// // var counter = 0;
+// // while (counter <= 10) {
+// //   console.log(counter);
+// //   counter++;
